b40c1651dba18d4ca873f2287fa8fd13924309ec
Pooya Parsa <pooya@pi0.io>
2025-05-14 01:31:34 +0200

    build: fix `/types` subpath

--- build.config.mjs
+++ build.config.mjs
@@ -1,17 +1,27 @@
 import { defineBuildConfig } from "obuild/config";
+import { rm } from "node:fs/promises";
+import { join } from "node:path";
 
 export default defineBuildConfig({
   entries: [
     {
       type: "bundle",
       input: [
-        "deno",
-        "bun",
-        "node",
-        "cloudflare",
-        "generic",
-        "service-worker",
-      ].map((adapter) => `src/adapters/${adapter}.ts`),
+        "src/types.ts",
+        ...[
+          "deno",
+          "bun",
+          "node",
+          "cloudflare",
+          "generic",
+          "service-worker",
+        ].map((adapter) => `src/adapters/${adapter}.ts`),
+      ],
     },
   ],
+  hooks: {
+    async end(ctx) {
+      await rm(join(ctx.pkgDir, "dist/types.mjs"));
+    },
+  },
 });
