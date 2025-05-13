c5003af0d45041e69dbe319fcd94f25de98ecebb
Pooya Parsa <pooya@pi0.io>
2025-05-14 00:14:19 +0200

    chore: update deno tests

--- test/deno.test.ts
+++ test/deno.test.ts
@@ -2,7 +2,7 @@ import { describe } from "vitest";
 import { testsExec } from "./_utils.ts";
 
 describe("deno", () => {
-  testsExec("deno run --unstable-byonm -A ./_fixture.ts", {
+  testsExec("deno run -A ./_fixture.ts", {
     runtime: "deno",
   });
 });
