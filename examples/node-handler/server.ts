890481aaa2a52b4be222c7de9bb450b16d00347e
Pooya Parsa <pooya@pi0.io>
2025-08-06 15:02:32 +0200

    add express example

--- examples/node-handler/server.ts
+++ examples/node-handler/server.ts
@@ -2,14 +2,6 @@ import type { IncomingMessage, ServerResponse } from "node:http";
 import { Readable } from "node:stream";
 
 export default function handler(req: IncomingMessage, res: ServerResponse) {
-  // res.end("OK");
   const stream = Readable.from(["Hello, ", "World!", "\n"]);
   stream.pipe(res);
 }
-
-// process
-//   .getBuiltinModule("node:http")
-//   .createServer(handler)
-//   .listen(3000, () => {
-//     console.log("Server is running on http://localhost:3000");
-//   });
