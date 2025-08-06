16439853e054226ac00410b8eb2f2a50e803582c
Pooya Parsa <pooya@pi0.io>
2025-08-06 16:20:00 +0200

    refactor(logger): better status colors

--- src/log.ts
+++ src/log.ts
@@ -4,13 +4,19 @@ import type { ServerMiddleware } from "./types.ts";
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface LogOptions {}
 
+const statusColors = { 1: "blue", 2: "green", 3: "yellow" } as Record<
+  number,
+  "blue" | "green" | "yellow"
+>;
+
 export const log = (_options: LogOptions = {}): ServerMiddleware => {
   return async (req, next) => {
     const start = performance.now();
     const res = await next();
     const duration = performance.now() - start;
+    const statusColor = statusColors[Math.floor(res.status / 100)] || "red";
     console.log(
-      `${c.gray(`[${new Date().toLocaleTimeString()}]`)} ${c.bold(req.method)} ${c.blue(req.url)} [${c[res.ok ? "green" : "red"](res.status + "")}] ${c.gray(`(${duration.toFixed(2)}ms)`)}`,
+      `${c.gray(`[${new Date().toLocaleTimeString()}]`)} ${c.bold(req.method)} ${c.blue(req.url)} [${c[statusColor](res.status + "")}] ${c.gray(`(${duration.toFixed(2)}ms)`)}`,
     );
     return res;
   };
