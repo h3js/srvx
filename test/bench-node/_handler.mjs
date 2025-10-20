d9b03ad268780c82b1f5823a13055bb27df1f4ac
Pooya Parsa <pooya@pi0.io>
2025-10-21 01:58:18 +0200

    perf(node): fast req body methods (#134)

--- test/bench-node/_handler.mjs
+++ test/bench-node/_handler.mjs
@@ -1,10 +1,34 @@
-export function fetchHandler(req) {
-  return new Response("Hello!", {
-    headers: { "x-test": req.headers.get("x-test") },
+import { json } from "node:stream/consumers";
+
+export async function fetchHandler(req) {
+  const body = await req.json();
+  return new Response(JSON.stringify(body), {
+    headers: {
+      "x-test": req.headers.get("x-test"),
+      "Content-Type": "application/json;charset=UTF-8",
+    },
   });
 }
 
-export function nodeHandler(_req, res) {
-  res.setHeader("x-test", _req.headers["x-test"] || "");
-  res.end("Hello!");
+export async function nodeHandler(req, res) {
+  const body = await readBody(req);
+  res.setHeader("x-test", req.headers["x-test"] || "");
+  res.setHeader("Content-Type", "application/json;charset=UTF-8");
+  res.end(JSON.stringify(body));
+}
+
+function readBody(req) {
+  return new Promise((resolve, reject) => {
+    const chunks = [];
+    req
+      .on("data", (chunk) => {
+        chunks.push(chunk);
+      })
+      .on("end", () => {
+        resolve(JSON.parse(Buffer.concat(chunks).toString("utf8") || "{}"));
+      })
+      .on("error", (err) => {
+        reject(err);
+      });
+  });
 }
