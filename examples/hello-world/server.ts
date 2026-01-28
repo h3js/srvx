aaeaa5d52720a4c6f436a7aeebb2f8790ccd9bc9
Pooya Parsa <pooya@pi0.io>
2026-01-29 00:59:54 +0100

    revert hello-world

--- examples/hello-world/server.ts
+++ examples/hello-world/server.ts
@@ -1,12 +1,7 @@
 export default {
   // https://srvx.h3.dev/guide/options
   port: 3000,
-  async fetch(req: Request) {
-    return Response.json({
-      url: req.url,
-      method: req.method,
-      body: await req.text(),
-      headers: Object.fromEntries(req.headers),
-    });
+  fetch(req: Request) {
+    return Response.json({ hello: "world!" });
   },
 };
