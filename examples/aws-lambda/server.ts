c1f9ee608e1c50e89c29fbc089f651bf395ac309
Pooya Parsa <pooya@pi0.io>
2026-02-04 12:54:53 +0100

    update example/docs

--- examples/aws-lambda/server.ts
+++ examples/aws-lambda/server.ts
@@ -1,6 +1,8 @@
 import { toLambdaHandler } from "srvx/aws-lambda";
+import { serveStatic } from "srvx/static";
 
 export const handler = toLambdaHandler({
+  middleware: [serveStatic({ dir: "public" })],
   fetch(req: Request) {
     return Response.json({ hello: "world!" });
   },
