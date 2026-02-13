4535696ab9f7255cf6a36ed658573af8fffea78f
Sandro Circi <sandro.circi@digitoolmedia.com>
2026-02-13 15:39:22 +0100

    fix(bunny): wait for ready when using Deno fallback

--- src/adapters/bunny.ts
+++ src/adapters/bunny.ts
@@ -114,6 +114,9 @@ class BunnyServer implements Server<(request: Request) => MaybePromise<Response>
   }
 
   ready(): Promise<Server<(request: Request) => MaybePromise<Response>>> {
+    if (this._denoServer) {
+      return this._denoServer.finished.then(() => this);
+    }
     return Promise.resolve(this);
   }
 
